031936e0a77fb0a4e07d79e2f89db64efa90b482
Sai Chandramouli <schand31@asu.edu>
2016-10-28 23:46:19 -0700

    starting with js stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= stub scripts
 //= require_tree .
+
---
+++ app/assets/javascripts/scripts.js
@@ -0,0 +1,7 @@
+/**
+ * Created by schandramouli on 10/28/16.
+ */
+
+$(document).ready(function() {
+
+});
